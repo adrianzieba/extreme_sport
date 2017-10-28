20527e899a7ee1a023bdcb2aebbf9f250d188340
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-10-29 00:17:54 +0200

    Filter routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -8,16 +8,23 @@ class RoutesController < ApplicationController
     render json: ::HashData::Route.new(route_id: params[:id]).perform
   end
 
+  def filter
+    filter_params = request.parameters.slice(
+      'prosthesis', 'crutches', 'wheelchairTetraplegia', 'wheelchairParaplegia'
+    ).select { |_, v| v }
+    render json: ::Route.where(filter_params).map(&:attributes)
+  end
+
   def create
 
   end
 
   def update
-    Route.find(params['id']).update_attributes(
-      prosthesis: params['prosthesis'],
-      crutches: params['crutches'],
-      wheelchair_tetraplegia: params['wheelchairTetraplegia'],
-      wheelchair_paraplegia: params['wheelchairParaplegia']
+    Route.find(request.parameters['id']).update_attributes(
+      prosthesis: request.parameters['prosthesis'],
+      crutches: request.parameters['crutches'],
+      wheelchair_tetraplegia: request.parameters['wheelchairTetraplegia'],
+      wheelchair_paraplegia: request.parameters['wheelchairParaplegia']
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get '/uploader', to:'fake#show'
 
   get '/routes', to: 'routes#index'
-  get '/routes/:id', to: 'routes#show'
+  get '/routes/:id', to: 'routes#show', constraints: { id: /\d/ }
   post 'routes', to: 'routes#create'
   put '/routes/:id', to: 'routes#update'
+  get '/routes/filter', to: 'routes#filter'
 
   resources :uploads, only: [:create]
 end
