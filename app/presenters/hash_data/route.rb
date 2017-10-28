3220ff9e5513b31bbd15129be49e6244682c3bce
Adrian Zięba <adrian.zieba@infakt.pl>
2017-10-28 19:51:07 +0200

    hash_data presenter

---
+++ app/presenters/hash_data/route.rb
@@ -0,0 +1,16 @@
+module HashData
+  class Route
+    def initialize(route: route)
+      @route = route
+    end
+
+    def perform
+      {
+
+      }
+    end
+
+    private
+    attr_reader :route
+  end
+end
