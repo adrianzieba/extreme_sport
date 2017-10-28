7aa45b3f4a8692cd20bdabdcd9c700d69844ee59
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-10-28 20:05:54 +0200

    Route + route points models; creator

---
+++ app/services/route_creator.rb
@@ -0,0 +1,35 @@
+class RouteCreator
+  def initialize(file_name)
+    @file_name = file_name
+  end
+
+  def perform
+    route = Route.create(
+      author: metadata.fetch('author'),
+      name: trk.fetch('name')
+    )
+
+    trk["trkseg"]["trkpt"].each_with_index do |point, index|
+      RoutePoint.create(lat: point["lat"], lon: point["lon"], order: index, route_id: route.id)
+    end
+  end
+
+  private
+  attr_reader :file_name
+
+  def trk
+    gpx.fetch('trk')
+  end
+
+  def metadata
+    gpx.fetch('metadata')
+  end
+
+  def gpx
+    hash.fetch("gpx")
+  end
+
+  def hash
+    @hash ||= Hash.from_xml(File.open(file_name, 'r').read)
+  end
+end
